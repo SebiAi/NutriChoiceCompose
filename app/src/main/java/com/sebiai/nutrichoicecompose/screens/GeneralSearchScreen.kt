08efc01a38e132da346ad9a1617cd12ace9b6d70
SebiAi <41116921+SebiAi@users.noreply.github.com>
2025-07-25 01:15:53 +0200

    Add the `LazyColumn` to the `GeneralSearchScreen` to test the integration
    App crashes when it tries to save the list of ingredients/meals because it can not marshal the classes... Why?

--- app/src/main/java/com/sebiai/nutrichoicecompose/screens/GeneralSearchScreen.kt
+++ app/src/main/java/com/sebiai/nutrichoicecompose/screens/GeneralSearchScreen.kt
@@ -1,12 +1,15 @@
 package com.sebiai.nutrichoicecompose.screens
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -17,6 +20,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -24,6 +28,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sebiai.nutrichoicecompose.R
 import com.sebiai.nutrichoicecompose.composables.FilterSearchBar
+import com.sebiai.nutrichoicecompose.composables.FoodCardBig
+import com.sebiai.nutrichoicecompose.dataclasses.Data
+import com.sebiai.nutrichoicecompose.dataclasses.Meal
 import com.sebiai.nutrichoicecompose.ui.theme.NutriChoiceComposeTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -32,7 +39,9 @@ fun GeneralSearchScreen(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.fillMaxWidth()
     ) {
+        // TODO: [Now + 1] Move to ViewModel bc. Ingredient class fails to pack into parcel
         var searchQuery by rememberSaveable { mutableStateOf("") }
+        var searchResults by rememberSaveable { mutableStateOf(listOf(Data.search("schn")[0])) }
 
         val searchBarOnSearch: (String) -> Unit = { query: String ->
             Log.d(null, "Searched with query \"$query\"")
@@ -70,6 +79,20 @@ fun GeneralSearchScreen(modifier: Modifier = Modifier) {
             fontWeight = FontWeight.Bold
         )
         Spacer(modifier = Modifier.height(6.dp))
+        LazyColumn (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(items = searchResults) {
+                FoodCardBig(
+                    image = it.getImage(LocalContext.current),
+                    title = it.title,
+                    priceString = it.getPriceString(LocalContext.current),
+                    isRestaurantFood = it is Meal,
+                    customizableChips = listOf() // TODO: [NOW + 2] Implement chips
+                )
+            }
+        }
     }
 }
 
