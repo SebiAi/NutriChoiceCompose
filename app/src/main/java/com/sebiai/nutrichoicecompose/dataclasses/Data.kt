d658f78e1b1d4853b6841a62cb431462cfa3aecf
SebiAi <41116921+SebiAi@users.noreply.github.com>
2025-07-25 16:52:15 +0200

    Add the rest of the test data

--- app/src/main/java/com/sebiai/nutrichoicecompose/dataclasses/Data.kt
+++ app/src/main/java/com/sebiai/nutrichoicecompose/dataclasses/Data.kt
@@ -108,10 +108,60 @@ object Data {
 
 
         val tomatoPaste = Ingredient("Tomato paste", R.drawable.tomato_paste_image, tomatoPasteNutrients, AFood.Price.LOW, AFood.Score.C, AFood.Score.C, AFood.DietaryPreferences.VEGAN)
-        // TODO: [Now] Add rest of the ingredients and meals
+        val beef = Ingredient("Beef", R.drawable.beef_image, beefNutrients, AFood.Price.MEDIUM, AFood.Score.B, AFood.Score.E, AFood.DietaryPreferences.NONE)
+        val pork = Ingredient("Pork", R.drawable.pork_image, porkNutrients, AFood.Price.MEDIUM, AFood.Score.C, AFood.Score.E, AFood.DietaryPreferences.NONE)
+        val carrots = Ingredient("Carrots", R.drawable.carrots_image, carrotsNutrients, AFood.Price.LOW, AFood.Score.A, AFood.Score.NA, AFood.DietaryPreferences.VEGAN)
+        val flour = Ingredient("Wheat Flour", R.drawable.wheat_flour_image, wheatNutrients, AFood.Price.LOW, AFood.Score.A, AFood.Score.A, AFood.DietaryPreferences.VEGAN)
+        val milk = Ingredient("Whole Milk", R.drawable.whole_milk_image, milkNutrients, AFood.Price.LOW, AFood.Score.C, AFood.Score.D, AFood.DietaryPreferences.VEGETARIAN)
+        val salt = Ingredient("Salt", R.drawable.salt_iamge, saltNutrients, AFood.Price.LOW, AFood.Score.NA, AFood.Score.A, AFood.DietaryPreferences.VEGAN)
+        val onion = Ingredient("Onion", R.drawable.onion_image, onionNutrients, AFood.Price.LOW, AFood.Score.NA, AFood.Score.NA, AFood.DietaryPreferences.VEGAN)
+        val vinegar = Ingredient("Vinegar", R.drawable.vinegar_image, vinegarNutrients, AFood.Price.LOW, AFood.Score.NA, AFood.Score.NA, AFood.DietaryPreferences.VEGAN)
+        val potato = Ingredient("Potato", R.drawable.potato_image, potatoNutrients, AFood.Price.LOW, AFood.Score.C, AFood.Score.D, AFood.DietaryPreferences.VEGAN)
+        val dumplingBread = Ingredient("Dumpling bread", R.drawable.dumpling_bread_image, breadcrumbsNutrients, AFood.Price.LOW, AFood.Score.NA, AFood.Score.NA, AFood.DietaryPreferences.VEGETARIAN)
+        val parsley = Ingredient("Parsley", R.drawable.parsley_image, parsleyNutrients, AFood.Price.LOW, AFood.Score.NA, AFood.Score.NA, AFood.DietaryPreferences.VEGAN)
+        val bacon = Ingredient("Bacon", R.drawable.bacon_image, baconNutrients, AFood.Price.LOW, AFood.Score.NA, AFood.Score.NA, AFood.DietaryPreferences.NONE)
+        val spaghetti = Ingredient("Spaghetti", R.drawable.spaghetti_image, spaghettiNutrients, AFood.Price.LOW, AFood.Score.A, AFood.Score.B, AFood.DietaryPreferences.VEGETARIAN)
+        val egg = Ingredient("Egg", R.drawable.egg_image, eggNutrients, AFood.Price.LOW, AFood.Score.NA, AFood.Score.NA, AFood.DietaryPreferences.VEGETARIAN)
+        val mozzarella = Ingredient("Mozzarella", R.drawable.mozzarella_image, mozzarellaNutrients, AFood.Price.LOW, AFood.Score.NA, AFood.Score.NA, AFood.DietaryPreferences.VEGETARIAN)
+        val tomato = Ingredient("Tomato", R.drawable.tomato_image, tomatoNutrients, AFood.Price.LOW, AFood.Score.NA, AFood.Score.NA, AFood.DietaryPreferences.VEGAN)
 
         testFoodSet.apply {
             add(tomatoPaste)
+            add(beef)
+            add(pork)
+            add(carrots)
+            add(flour)
+            add(milk)
+            add(salt)
+            add(onion)
+            add(vinegar)
+            add(potato)
+            add(dumplingBread)
+            add(parsley)
+            add(bacon)
+            add(spaghetti)
+            add(egg)
+            add(mozzarella)
+            add(tomato)
+        }
+
+        val lasagneIngredients: List<AFood> = listOf(tomatoPaste, beef, pork, carrots, flour, milk, salt)
+        val schnitzelIngredients: List<AFood> = listOf(pork, flour, salt)
+        val potatoSaladIngredients: List<AFood> = listOf(vinegar, onion, salt, potato)
+        val dumplingIngredients: List<AFood> = listOf(parsley, dumplingBread, salt, bacon)
+        val carbonaraIngredients: List<AFood> = listOf(egg, spaghetti, salt, bacon)
+        val margheritaIngredients: List<AFood> = listOf(flour, tomato, salt, mozzarella)
+
+        val uniWien = Mensa("Mensa Uni Wien")
+        val pfarrwirt = Mensa("Pfarrwirt")
+
+        testFoodSet.apply {
+            add(Meal("Lasagne", R.drawable.lasagna_image, lasagneNutrients, AFood.Price.MEDIUM, AFood.Score.C, AFood.Score.D, AFood.DietaryPreferences.NONE, lasagneIngredients, uniWien))
+            add(Meal("Wiener Schnitzel", R.drawable.wiener_schnitzl_image, schnitzelNutrients, AFood.Price.MEDIUM, AFood.Score.B, AFood.Score.D, AFood.DietaryPreferences.NONE, schnitzelIngredients, pfarrwirt))
+            add(Meal("Potato salad", R.drawable.potato_salad_image, potatoSaladNutrients, AFood.Price.MEDIUM, AFood.Score.B, AFood.Score.D, AFood.DietaryPreferences.VEGETARIAN, potatoSaladIngredients, pfarrwirt))
+            add(Meal("Bacon dumpling", R.drawable.bacon_dumpling_image, dumplingNutrients, AFood.Price.MEDIUM, AFood.Score.C, AFood.Score.NA, AFood.DietaryPreferences.NONE, dumplingIngredients, pfarrwirt))
+            add(Meal("Spaghetti alla carbonara", R.drawable.carbonara_image, carbonaraNutrients, AFood.Price.MEDIUM, AFood.Score.C, AFood.Score.C, AFood.DietaryPreferences.NONE, carbonaraIngredients, uniWien))
+            add(Meal("Pizza margherita", R.drawable.margherita_image, margheritaNutrients, AFood.Price.MEDIUM, AFood.Score.C, AFood.Score.A, AFood.DietaryPreferences.VEGETARIAN, margheritaIngredients, uniWien))
         }
     }
 
