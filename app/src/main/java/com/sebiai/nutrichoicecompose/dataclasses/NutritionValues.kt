fb4d591cd607c4d70289e8fac32a9d5d3443c4fa
SebiAi <41116921+SebiAi@users.noreply.github.com>
2025-07-27 17:50:11 +0200

    Default contstructor for `NutritionValues`

--- app/src/main/java/com/sebiai/nutrichoicecompose/dataclasses/NutritionValues.kt
+++ app/src/main/java/com/sebiai/nutrichoicecompose/dataclasses/NutritionValues.kt
@@ -9,4 +9,16 @@ data class NutritionValues(
     val carbs: Double,
     val salt: Double,
     val sugar: Double,
-) : Serializable
+) : Serializable {
+
+    init {
+        assert(calories >= 0.0)
+        assert(protein >= 0.0)
+        assert(fat >= 0.0)
+        assert(carbs >= 0.0)
+        assert(salt >= 0.0)
+        assert(sugar >= 0.0)
+    }
+
+    constructor() : this(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+}
