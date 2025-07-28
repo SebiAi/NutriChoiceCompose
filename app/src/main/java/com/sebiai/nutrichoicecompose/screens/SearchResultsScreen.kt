5f900ef83e9d2d26e24c9f1b837a3f61b213feec
SebiAi <41116921+SebiAi@users.noreply.github.com>
2025-07-28 20:23:37 +0200

    Add info text when there are no search results

--- app/src/main/java/com/sebiai/nutrichoicecompose/screens/SearchResultsScreen.kt
+++ app/src/main/java/com/sebiai/nutrichoicecompose/screens/SearchResultsScreen.kt
@@ -3,6 +3,7 @@ package com.sebiai.nutrichoicecompose.screens
 import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +17,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -25,6 +27,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sebiai.nutrichoicecompose.R
+import com.sebiai.nutrichoicecompose.composables.CenteredTitleWithSubtitle
 import com.sebiai.nutrichoicecompose.composables.FilterSearchBar
 import com.sebiai.nutrichoicecompose.composables.FoodCard
 import com.sebiai.nutrichoicecompose.composables.FoodCardType
@@ -69,26 +72,43 @@ fun SearchResultsScreen(
             fontWeight = FontWeight.Bold
         )
         Spacer(modifier = Modifier.height(6.dp))
-        LazyColumn (
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            state = uiState.resultScrollState
-        ) {
-            items(items = uiState.searchResults) {
-                FoodCard(
-                    modifier = Modifier.clickable(
-                        onClick = {
-                            viewModel.addRecentlyViewedFood(it)
-                            onFoodCardClicked(it, sharedUiState.nutritionPreferences)
-                        }
-                    ),
-                    type = FoodCardType.BIG,
-                    image = it.getImage(LocalContext.current),
-                    title = it.title,
-                    priceString = it.getPriceString(LocalContext.current),
-                    isRestaurantFood = it is Meal,
-                    customizableChips = determineCustomizableChips(LocalContext.current, it, sharedUiState.nutritionPreferences)
+        if (uiState.searchResults.isEmpty()) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CenteredTitleWithSubtitle(
+                    modifier = Modifier.padding(8.dp, 0.dp),
+                    title = stringResource(R.string.no_search_results_heading),
+                    subtitle = stringResource(R.string.no_search_results_subtitle),
                 )
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                state = uiState.resultScrollState
+            ) {
+                items(items = uiState.searchResults) {
+                    FoodCard(
+                        modifier = Modifier.clickable(
+                            onClick = {
+                                viewModel.addRecentlyViewedFood(it)
+                                onFoodCardClicked(it, sharedUiState.nutritionPreferences)
+                            }
+                        ),
+                        type = FoodCardType.BIG,
+                        image = it.getImage(LocalContext.current),
+                        title = it.title,
+                        priceString = it.getPriceString(LocalContext.current),
+                        isRestaurantFood = it is Meal,
+                        customizableChips = determineCustomizableChips(
+                            LocalContext.current,
+                            it,
+                            sharedUiState.nutritionPreferences
+                        )
+                    )
+                }
             }
         }
     }
