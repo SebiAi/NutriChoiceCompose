756b3e69ca6be034e99b81491bfeb00585fa144c
SebiAi <41116921+SebiAi@users.noreply.github.com>
2025-08-02 16:26:21 +0200

    Add missing preview to `RestaurantIndicatorInfoDialog`

--- app/src/main/java/com/sebiai/nutrichoicecompose/composables/RestaurantIndicatorInfoDialog.kt
+++ app/src/main/java/com/sebiai/nutrichoicecompose/composables/RestaurantIndicatorInfoDialog.kt
@@ -6,7 +6,9 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.sebiai.nutrichoicecompose.R
+import com.sebiai.nutrichoicecompose.ui.theme.NutriChoiceComposeTheme
 
 @Composable
 fun RestaurantIndicatorInfoDialog(
@@ -25,4 +27,14 @@ fun RestaurantIndicatorInfoDialog(
             ) { Text(text = stringResource(R.string.ok_confirmation)) }
         }
     )
+}
+
+@PreviewLightDark
+@Composable
+private fun RestaurantIndicatorInfoDialogPreview() {
+    NutriChoiceComposeTheme {
+        RestaurantIndicatorInfoDialog(
+            onDismiss = {}
+        )
+    }
 }
